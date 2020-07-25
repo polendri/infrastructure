c092a24c0779096a2ec88e30615d6e13ae2566c2
Paul Hendry <paul@pshendry.com>
2020-07-25 15:11:06 -0400

    home.pshendry.com: Add Transmission service, fix Wireguard config

--- playbooks/roles/home.pshendry.com/files/wireguard/wg1-down.sh
+++ playbooks/roles/home.pshendry.com/files/wireguard/wg1-down.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # {{ansible_managed | comment}}
 
+ip rule delete from 0/0 to 0/0 table 200
+ip route flush cache
 ip link delete dev wg1
 resolvconf -d wg1 -f
--- playbooks/roles/home.pshendry.com/templates/wireguard/wg1-up.sh
+++ playbooks/roles/home.pshendry.com/templates/wireguard/wg1-up.sh
@@ -6,3 +6,6 @@ wg setconf wg1 /etc/wireguard/wg1.conf
 ip address add {{wg1_local_ipv4}}/32 dev wg1
 ip link set up dev wg1
 echo "nameserver {{wg1_dns_ip}}\n" | resolvconf -a wg1 -m 0 -x
+ip rule add from {{wg1_local_ipv4}}/32 table 200
+ip route add default via {{wg1_local_ipv4}} dev wg1 table 200
+ip route flush cache
